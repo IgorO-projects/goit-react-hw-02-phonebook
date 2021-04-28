8ff73148c244e8d3623ba04e7d1674acd9f61cbe
Igor <Travis789@gmail.com>
2021-04-28 08:12:44 +0200

    fixedForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -26,19 +26,19 @@ class ContactForm extends Component {
   render() {
 
     return (
-      <form>
+      <form onSubmit={this.onSubmit} >
         <label>
           <br/>
           <input
           className={styles.input}
           type="text"
           name="name"
-          value={this.state.name}
-          onChange={this.handleChange}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-          required
           placeholder="Name"
+          required
+          value={this.state.name}
+          onChange={this.handleChange}
           />
         </label>
         <br/>
@@ -48,18 +48,17 @@ class ContactForm extends Component {
           className={styles.input}
           type="tel"
           name="number"
-          value={this.state.number}
-          onChange={this.handleChange}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-          required
           placeholder="Number"
+          required
+          value={this.state.number}
+          onChange={this.handleChange}
           />
         </label>
         <br/>
         <button
         className={styles.button}
-        onClick={this.onSubmit} 
         type="submit"
         >Add contact</button>
       </form>
